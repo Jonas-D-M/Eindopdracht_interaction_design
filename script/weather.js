cb28e0f6acd76967bde3a853fa9eccbdc2f6a7e4
De Meyer Jonas <jonas.de.meyer@student.howest.be>
2019-11-15 15:27:20 +0100

    got temps from API

--- script/weather.js
+++ script/weather.js
@@ -1,4 +1,6 @@
-serverEndpoint = 'https://api.nasa.gov/insight_weather/?api_key=MvvlPiedxmmIBY45P0Dboo28sPGbopdXaZ7iqamW&feedtype=json&ver=1.0';
+// Documentation: https://api.nasa.gov/assets/insight/InSight%20Weather%20API%20Documentation.pdf
+serverEndpoint =
+  "https://api.nasa.gov/insight_weather/?api_key=MvvlPiedxmmIBY45P0Dboo28sPGbopdXaZ7iqamW&feedtype=json&ver=1.0";
 
 fetch(serverEndpoint)
   .then(function(response) {
@@ -7,17 +9,59 @@ fetch(serverEndpoint)
   })
   .then(function(json) {
     console.log(JSON.stringify(json));
-    showWeather(json);
+    getSolKeys(json);
+    // getTemperatures(json);
   });
 
-const showWeather = function(json) {
+const getSolKeys = function(json) {
   let keys = [];
-  console.log('Callback is succesful');
-  for (let days of json['sol_keys']) {
+  let av_temps = [];
+  let mn_temps = [];
+  let mx_temps = [];
+
+  // Gets keys to access temps
+  for (let days of json.sol_keys) {
     keys.push(days);
   }
+
+  // Gets the different temps with the keys
+  for (let i = 0; i < keys.length; i++) {
+    av_temp = json[keys[i]].AT.av;
+    // conversion to celcius
+    c_temp = (av_temp - 32) * 0.5556;
+    av_temps.push(c_temp);
+  }
+  for (let i = 0; i < keys.length; i++) {
+    mn_temp = json[keys[i]].AT.mn;
+    c_temp = (mn_temp - 32) * 0.5556;
+    mn_temps.push(c_temp);
+  }
+  for (let i = 0; i < keys.length; i++) {
+    mx_temp = json[keys[i]].AT.mx;
+    c_temp = (mx_temp - 32) * 0.5556;
+    mx_temps.push(c_temp);
+  }
+
+  console.log("av temps");
+  console.log(av_temps);
+  console.log("mn temps");
+  console.log(mn_temps);
+  console.log("mx temps");
+  console.log(mx_temps);
 };
 
-document.addEventListener('DOMContentLoaded', function() {
-  console.log('JAVASCRIPT IS LOADED!');
+// JSON.259.AT.av
+// vb. 259.AT.av = 71.233 °C
+
+// const getTemperatures = function(json) {
+//   console.log("Here are the temps");
+//   for (let days of json.sol_keys) {
+//     temps.push(days);
+//     console.log(days);
+//     console.log("Temp: ", days);
+//   }
+// };
+
+document.addEventListener("DOMContentLoaded", function() {
+  console.log("JAVASCRIPT IS LOADED!");
 });
